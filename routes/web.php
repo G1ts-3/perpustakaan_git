045aea32a241a4755185565943a6b274c78e31f4
Git <raghidmuhammad3@gmail.com>
2026-05-09 20:36:30 +0700

    fix : login page

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
+
+Route::livewire('/login', 'login-component')->name('login');
